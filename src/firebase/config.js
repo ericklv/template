5f61dd2d66341810a81441653f52090b81bcaeb4
Erick <elazon@gmail.com>
2019-07-12 20:13:50 -0500

    remove api key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,12 +10,7 @@ const productionConfig = {
 };
 
 const devConfig = {
-    // apiKey: "AIzaSyCDDvGT5jLAuH8oXPaj1eqFkeZHKLbvXWg",
-    // authDomain: "blank-project-225314.firebaseapp.com",
-    // databaseURL: "https://blank-project-225314.firebaseio.com",
-    // projectId: "blank-project-225314",
-    // storageBucket: "blank-project-225314.appspot.com",
-    // messagingSenderId: "606785281720"
+
 };
 
 if (hostName === "url-prod") {
