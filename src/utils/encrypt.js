e740c2381b12691f20714eca5f611c1c0e761f69
Erick <elazon@gmail.com>
2019-07-16 12:41:56 -0500

    remove unused dependencies

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,5 +1,4 @@
 import Enc from 'enc';
-import CryptoAES from 'crypto-js/aes';
 
 export const encrypt = (value) => {
     return Enc.base64.encode(value);
@@ -7,20 +6,4 @@ export const encrypt = (value) => {
 
 export const decrypt = (value) => {
     return Enc.base64.decode(value);
-};
-
-export const encryptAES = (message, key) => {
-    return CryptoAES.encrypt(message, key).toString()
-};
-
-export const decryptAES = (cryptomessage, key) => {
-    return hex2string(CryptoAES.decrypt(cryptomessage, key).toString()).toString()
-};
-
-export const hex2string = (hexx) => {
-    let hex = hexx.toString();
-    let str = '';
-    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
 };
