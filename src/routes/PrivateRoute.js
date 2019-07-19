4a1b52021849d2d9722e6deb80eb9322f9d72af1
Erick <elazon@gmail.com>
2019-07-19 14:44:10 -0500

    fix scroll Top

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -3,9 +3,14 @@ import {Route} from "react-router-dom";
 
 export default class PrivateRoute extends Component {
 
+    componentDidUpdate = (prevProps) => {
+        if(this.props.location !== prevProps.location) window.scrollTo(0,0);
+    };
+
     childWithProperties = (props) => React.Children.map(this.props.children, child => React.cloneElement(child, {...props}));
 
     render() {
+        console.log(this.props);
         return (
 
             <Route exact
